9f394700fdc301a93e348ee2921eaab2b28fbe84
Alexand <alex.zephaniah244@gmail.com>
2019-05-17 22:13:04 +0300

    Commit

--- project.js
+++ project.js
@@ -9,15 +9,19 @@ console.log(time);
           optionalExpenses: {},
           income: [],
           savings: false
-
- }
- let a1 = prompt("Enter an expense");
-  let a2 = prompt("How much is?");   
-  let a3 = prompt("Enter an expense");
-  let a4 = prompt("How much is?");   
-
-  AppData.expenses.a1 = a2;
-  AppData.expenses.a3 = a4;
-
-  alert(AppData.budget/30);
-  
+ };
+   
+            for(let i = 0; i < 2; i++)
+            {
+                let a = prompt("Enter an expense");
+                let b = prompt("How much is?");  
+                if ((typeof(a)) === 'string' && (typeof(b)) === 'string' && (typeof(a)) != null && (typeof(b)) != null 
+                && a != '' && b != '' && a.length < 50 && a.length < 50 && b.length < 50) 
+                   {
+                      console.log ("done");
+                      AppData.expenses[a] = b;
+                   } else {
+                    i = i - 1;
+                   }
+            }
+    alert(AppData.budget/30);
